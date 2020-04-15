969fad3f3b9dd6664e4a32d60b8b0fa072a72071
j-rockwell <jrobertrockwell@gmail.com>
2020-04-14 20:07:21 -0700

    Added chat link posting restrictions

--- src/main/java/com/llewkcor/ares/humbug/cont/mods/ChatMod.java
+++ src/main/java/com/llewkcor/ares/humbug/cont/mods/ChatMod.java
@@ -1,16 +1,22 @@
 package com.llewkcor.ares.humbug.cont.mods;
 
+import com.llewkcor.ares.commons.event.ProcessedChatEvent;
 import com.llewkcor.ares.commons.util.general.Configs;
 import com.llewkcor.ares.humbug.Humbug;
 import com.llewkcor.ares.humbug.cont.HumbugMod;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.List;
 
 public final class ChatMod implements HumbugMod, Listener {
     @Getter public final Humbug plugin;
@@ -18,6 +24,8 @@ public final class ChatMod implements HumbugMod, Listener {
     @Getter @Setter public boolean enabled;
 
     @Getter public boolean hideJoinLeaveMessages;
+    @Getter public boolean disablePostingLinks;
+    @Getter public List<String> whitelistedLinks;
 
     public ChatMod(Humbug plugin) {
         this.plugin = plugin;
@@ -30,13 +38,33 @@ public final class ChatMod implements HumbugMod, Listener {
     public void load() {
         final YamlConfiguration config = Configs.getConfig(plugin, "config");
 
-        this.enabled = config.getBoolean("mods.chat.enabled");
         this.hideJoinLeaveMessages = config.getBoolean("mods.chat.hide_join_leave_messages");
+        this.disablePostingLinks = config.getBoolean("mods.chat.disable_posting_links");
+        this.whitelistedLinks = config.getStringList("mods.chat.allowed_links");
+
+        this.enabled = true;
     }
 
     @Override
     public void unload() {
         this.enabled = false;
+    }
+
+    /**
+     * Returns true if the provided string is a blacklisted link
+     * @param message Message
+     * @return True if blacklisted
+     */
+    private boolean isBlacklistedLink(String message) {
+        final boolean match = message.matches("^(http://www\\.|https://www\\.|http://|https://)?[a-z0-9]+([\\-.][a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$");
+
+        for (String whitelisted : whitelistedLinks) {
+            if (message.contains(whitelisted)) {
+                return false;
+            }
+        }
+
+        return match;
     }
 
     @EventHandler
@@ -50,6 +78,28 @@ public final class ChatMod implements HumbugMod, Listener {
     public void onPlayerQuit(PlayerQuitEvent event) {
         if (isEnabled() && isHideJoinLeaveMessages()) {
             event.setQuitMessage(null);
+        }
+    }
+
+    @EventHandler (priority = EventPriority.HIGHEST)
+    public void onChat(ProcessedChatEvent event) {
+        if (!isEnabled() || !isDisablePostingLinks() || event.isCancelled()) {
+            return;
+        }
+
+        final Player player = event.getPlayer();
+        final String message = event.getMessage();
+        final String[] split = message.split(" ");
+
+        for (String str : split) {
+            if (isBlacklistedLink(str)) {
+
+                player.sendMessage(ChatColor.RED + "This type of link is blacklisted for non-premium users. Purchase a rank at " + ChatColor.AQUA +
+                        "https://playares.com/store" + ChatColor.RED + " to bypass this restriction");
+
+                event.setCancelled(true);
+                return;
+            }
         }
     }
 }
