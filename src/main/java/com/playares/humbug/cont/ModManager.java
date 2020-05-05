0ab40f5f62078907831517f9b5d7da9c6c8da8c5
j-rockwell <jrobertrockwell@gmail.com>
2020-05-04 17:43:59 -0700

    Improved Humbug Mod management

--- src/main/java/com/playares/humbug/cont/ModManager.java
+++ src/main/java/com/playares/humbug/cont/ModManager.java
@@ -1,44 +1,48 @@
 package com.playares.humbug.cont;
 
-import com.google.common.collect.Sets;
+import com.google.common.collect.Maps;
 import com.playares.humbug.HumbugService;
 import com.playares.humbug.cont.mods.*;
 import lombok.Getter;
 
-import java.util.Set;
+import java.util.Map;
 
 public final class ModManager {
     @Getter public HumbugService plugin;
-    @Getter public Set<HumbugMod> mods;
+    @Getter public Map<Class<? extends HumbugMod>, HumbugMod> mods;
 
     public ModManager(HumbugService plugin) {
         this.plugin = plugin;
-        this.mods = Sets.newConcurrentHashSet();
+        this.mods = Maps.newConcurrentMap();
 
-        mods.add(new WorldMod(plugin));
-        mods.add(new ElevatorMod(plugin));
-        mods.add(new MobstackMod(plugin));
-        mods.add(new KitMod(plugin));
-        mods.add(new KnockbackMod(plugin));
-        mods.add(new PotionMod(plugin));
-        mods.add(new XPMod(plugin));
-        mods.add(new MiningMod(plugin));
-        mods.add(new BotMod(plugin));
-        mods.add(new ChatMod(plugin));
-        mods.add(new ExploitMod(plugin));
-        mods.add(new DurabilityMod(plugin));
-        mods.add(new AttributeMod(plugin));
+        mods.put(WorldMod.class, new WorldMod(plugin));
+        mods.put(ElevatorMod.class, new ElevatorMod(plugin));
+        mods.put(MobstackMod.class, new MobstackMod(plugin));
+        mods.put(KitMod.class, new KitMod(plugin));
+        mods.put(KnockbackMod.class, new KnockbackMod(plugin));
+        mods.put(PotionMod.class, new PotionMod(plugin));
+        mods.put(XPMod.class, new XPMod(plugin));
+        mods.put(MiningMod.class, new MiningMod(plugin));
+        mods.put(BotMod.class, new BotMod(plugin));
+        mods.put(ChatMod.class, new ChatMod(plugin));
+        mods.put(ExploitMod.class, new ExploitMod(plugin));
+        mods.put(DurabilityMod.class, new DurabilityMod(plugin));
+        mods.put(AttributeMod.class, new AttributeMod(plugin));
+    }
+
+    public HumbugMod getMod(Class<? extends HumbugMod> clazz) {
+        return mods.get(clazz);
     }
 
     public void load() {
-        mods.forEach(HumbugMod::load);
+        mods.values().forEach(HumbugMod::load);
     }
 
     public void unload() {
-        mods.forEach(HumbugMod::unload);
+        mods.values().forEach(HumbugMod::unload);
     }
 
     public void reload() {
-        mods.forEach(HumbugMod::reload);
+        mods.values().forEach(HumbugMod::reload);
     }
 }
